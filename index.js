b664a5fb68e5dc39040e265c3a0aa4f5e998ca2c
helenpidoulas <helen.pidoulas@gmail.com>
2019-03-17 20:33:26 +1100

    progress commit - simple node.js weather app

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+// OpenWeatherMap API call
+let request = require('request');
+
+// OpenWeatherMap API key
+// variable = apiKey
+// assigned it to string value of API Key
+let apiKey = '8a1a3cc956c204ebbbd40834bdd9dc38';
+
+// Created a city and assigned it a string value
+// of the city of Sydney to test with
+let city = 'Sydney';
+
+// Created a variable named url
+// assigned it to OpenWeatherMap URL
+// two query parameters:
+// 1. city
+// 2. apiKey
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+
 // pass in target url & request returns callback function
 // require request package
-const request = require('request');
+//const request = require('request');
 
 // request returns a callback function
 // arguments: err, response, and body.
